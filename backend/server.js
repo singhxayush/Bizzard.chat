032c8e4b82f576d3398c4045841a15bd9589dba2
singhxayush <fanduayush2000@gmail.com>
2024-02-24 01:17:55 +0530

    Updates server.js with all the route and other changes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 //--> Package Imports
 import express from "express";
 import dotenv from "dotenv";
-// import bodyParser from 'body-parser'
-// app.use(bodyParser.json())
+import cookieParser from 'cookie-parser';
 
 //--> File Imports
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
 import connectToMongoDB from "./database/connectToMongoDB.js";
 
 const PORT = process.env.PORT || 5000;
@@ -14,15 +14,29 @@ const app = express();
 
 dotenv.config();
 
+/*
+Developers Guide:
+
+--> Routing API's
+
+
+*/
+
 // ---------[MIDDLEWARES]---------
 // A middlware to extract these fields {fullName, userName, password, confirmPassword, gender} from req.body under auth.controller.js
 app.use(express.json()); // to parse the incoming request with the JSON payload (req.body)
 
+app.use(cookieParser());
+
 // Authentication Routes MiddleWare
 app.use("/api/auth", authRoutes);
 
-
+// 
 app.use("/api/messages", messageRoutes);
+
+// 
+app.use("/api/users", userRoutes);
+
 // -------------------------------
 
 
